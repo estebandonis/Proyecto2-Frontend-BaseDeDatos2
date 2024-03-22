b207b349ea0d5525e262f84cb61fd8bdb5cbaa7b
Esteban Donis <estebandonis10@gmail.com>
2024-03-22 17:11:07 -0600

    Conexión con neo4j lograda

--- src/hooks/useApi.js
+++ src/hooks/useApi.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-const apiUrl = "http://localhost:3000/api";
+const apiUrl = "http://127.0.0.1:5000/api";
 
 const useApi = () => {
   const [data, setData] = useState(null);
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,30 @@
 import { navigate } from "@store";
 import stiles from "./Home.module.css";
+import { useEffect } from "react";
+
+import { useApi } from "@hooks";
+import axios from "axios";
 
 const Home = () => {
+
+  const { apiUrl } = useApi();
+
+  const getData = async () => {
+    await axios
+      .get(`${apiUrl}/info`)
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        // Handle the error
+        console.log("An error occurred while retrieving data", error);
+      });
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <div className={stiles.bigStyles}>
       <div className={stiles.styles}>
