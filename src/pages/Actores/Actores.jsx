643529c92e0cd610512478f50e4abe611fad482a
GarciaAlegria <abnerivan2003@gmail.com>
2024-04-12 16:53:06 -0600

    actualizacion actores

--- src/pages/Actores/Actores.jsx
+++ src/pages/Actores/Actores.jsx
@@ -1,9 +1,40 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useApi } from "@hooks";
+import styles from './Actores.module.css';
+
 const Actores = () => {
-    return (
-        <div>
-        <h1>Actores</h1>
-        </div>
-    );
-    }
+    const { apiUrl } = useApi();
+    const [actores, setActores] = useState([]);
+
+
+  return (
+    <div>
+      <h1>Actores</h1>
+      <table className={styles.actorTable}>
+        <thead>
+          <tr>
+            <th>Nombre</th>
+            <th>Apellido</th>
+            <th>Año de nacimiento</th>
+            <th>Nacionalidad</th>
+            <th>Premios</th>
+          </tr>
+        </thead>
+        <tbody>
+          {actores.map((actor, index) => (
+            <tr key={index}>
+              <td>{actor.nombre}</td>
+              <td>{actor.apellido}</td>
+              <td>{actor.yearBorn}</td>
+              <td>{actor.nacionalidad}</td>
+              <td>{actor.premios}</td>  
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
 export default Actores;
