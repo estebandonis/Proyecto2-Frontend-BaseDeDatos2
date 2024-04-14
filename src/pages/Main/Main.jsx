efdeea3894cfdf5a6bbe1db305d73ea75280cb8b
Esteban Donis <estebandonis10@gmail.com>
2024-04-13 22:18:29 -0600

    Agregue ordenamiento de peliculas

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -10,12 +10,12 @@ import stiles from "./Main.module.css";
 const Main = () => {
 
   const { apiUrl } = useApi();
-
   const { user } = useStoreon('user');
 
   const [searchTerm, setSearchTerm] = useState("");
   const [pelicula, setPelicula] = useState([]);
   const [peliculas, setPeliculas] = useState([]);
+  const [desc, setDesc] = useState(true)
 
   const handleSearchChange = (event) => {
     setSearchTerm(event.target.value);
@@ -35,16 +35,18 @@ const Main = () => {
   };
 
   const getPeliculasByGenres = async (preferencia) => {
-    await axios
-      .post(`${apiUrl}/peliculas/getMovieGenero`, {genero: preferencia})
-      .then((response) => {
-        console.log(response.data);
-        setPeliculas([...peliculas, ...response.data]);
-      })
-      .catch((error) => {
-        // Handle the error
-        console.log("An error occurred while retrieving data", error);
-      });
+    if (preferencia !== undefined) {
+      await axios
+        .post(`${apiUrl}/peliculas/getMovieGenero`, {genero: preferencia, desc: desc})
+        .then((response) => {
+          console.log(response.data);
+          setPeliculas([...peliculas, ...response.data]);
+        })
+        .catch((error) => {
+          // Handle the error
+          console.log("An error occurred while retrieving data", error);
+        });
+    }
   };
 
   const getPeliculasByDirector = async (preferencia) => {
@@ -75,15 +77,41 @@ const Main = () => {
 
   const getRecomendations = async () => {
     user.preferencias.forEach(element => {
-      getPeliculasByGenres(element)
+      if (element !== undefined) {
+        getPeliculasByGenres(element)
+      }
     })
   }
 
+  const orderPeliculas = () => {
+    if (desc) {
+      console.log(peliculas.sort((a, b) => (a.averRating > b.averRating) ? 1 : -1))
+      setPeliculas(peliculas.sort((a, b) => (a.averRating > b.averRating) ? 1 : -1))
+    }
+    else {
+      console.log(peliculas.sort((a, b) => (a.averRating < b.averRating) ? 1 : -1))
+      setPeliculas(peliculas.sort((a, b) => (a.averRating < b.averRating) ? 1 : -1))
+    }
+  }
 
+  const handleDescChange = (event) => {
+    if (event.target.value === "true") {
+      setPeliculas([])
+      setDesc(true)
+    }
+    else {
+      setPeliculas([])
+      setDesc(false)
+    }
+  }
 
   useEffect(() => {
     getRecomendations();
   }, []);
+
+  useEffect(() => {
+    getRecomendations();
+  }, [desc])
 
   return (
     <div className={stiles.bigStyles}>
@@ -108,12 +136,15 @@ const Main = () => {
                 sinopsis={pelicula.sinopsis}
                 titulo={pelicula.titulo}
                 year={pelicula.year}
-                averRating={pelicula.averRating}
               />
             ))}
           </div> : null}
           
           <h1>Recomendaciones basandonos en tus generos preferidos</h1>
+          <select onChange={handleDescChange}>
+            <option value="true">Descendente</option>
+            <option value="false">Ascendente</option>
+          </select>
           {peliculas.length !== 0 ?
           <div className={stiles.listMovies}>
             {peliculas.map((pelicula, index) => (
