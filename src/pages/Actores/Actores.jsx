2e227098c611d47da0b989aaed20ca8fbc2d797f
GarciaAlegria <abnerivan2003@gmail.com>
2024-04-15 13:08:51 -0600

    crear actor

--- src/pages/Actores/Actores.jsx
+++ src/pages/Actores/Actores.jsx
@@ -9,18 +9,27 @@ const Actores = () => {
     const [nombreActorAEliminar, setNombreActorAEliminar] = useState('');
     const [apellidoActorAEliminar, setApellidoActorAEliminar] = useState('');
     const [showEliminarInput, setShowEliminarInput] = useState(false);
+    const [showCrearActorInputs, setShowCrearActorInputs] = useState(false);
+    const [nuevoActor, setNuevoActor] = useState({
+        nombre: '',
+        apellido: '',
+        yearBorn: '',
+        nacionalidad: '',
+        premios: ''
+    });
+
+    // Definir la función fetchActores fuera de useEffect
+    const fetchActores = async () => {
+        try {
+            const response = await axios.get(`${apiUrl}/actores/info`);
+            setActores(response.data);
+        } catch (error) {
+            console.error('Error al cargar actores:', error);
+        }
+    };
 
     useEffect(() => {
-        const fetchActores = async () => {
-            try {
-                const response = await axios.get(`${apiUrl}/actores/info`);
-                setActores(response.data);
-            } catch (error) {
-                console.error('Error al cargar actores:', error);
-            }
-        };
-
-        fetchActores();
+        fetchActores(); // Llamar a fetchActores cuando cambia apiUrl
     }, [apiUrl]);
 
     const handleEliminarActor = async () => {
@@ -33,58 +42,70 @@ const Actores = () => {
                 }
             });
             console.log(response.data); // Mensaje de confirmación de eliminación
-    
+
             // Mostrar alerta de éxito
             alert(`Se eliminó el actor ${nombreActorAEliminar} ${apellidoActorAEliminar}`);
-    
+
             // Actualizar la lista de actores después de eliminar el nodo
-            const updatedActores = actores.filter(actor => actor.nombre !== nombreActorAEliminar || actor.apellido !== apellidoActorAEliminar);
+            const updatedActores = actores.filter(
+                (actor) => actor.nombre !== nombreActorAEliminar || actor.apellido !== apellidoActorAEliminar
+            );
             setActores(updatedActores);
-    
+
             // Limpiar el campo de entrada y ocultar el input
             setNombreActorAEliminar('');
             setApellidoActorAEliminar('');
             setShowEliminarInput(false);
         } catch (error) {
             console.error('Error al eliminar actor:', error);
-    
+
             // Mostrar alerta de error
             alert(`Error al eliminar el actor ${nombreActorAEliminar} ${apellidoActorAEliminar}`);
         }
     };
-    
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setNuevoActor({ ...nuevoActor, [name]: value });
+    };
+
+    const handleCrearNuevoActor = async () => {
+        try {
+            const response = await axios.post(`${apiUrl}/actores/create_node_with_properties`, {
+                label: 'Actor',
+                properties: nuevoActor
+            });
+            console.log(response.data); // Mensaje de confirmación de creación
+
+            // Mostrar alerta de éxito
+            alert('¡Actor creado exitosamente!');
+
+            // Limpiar los campos de entrada y ocultar los inputs
+            setNuevoActor({
+                nombre: '',
+                apellido: '',
+                yearBorn: '',
+                nacionalidad: '',
+                premios: ''
+            });
+            setShowCrearActorInputs(false);
+
+            // Actualizar la lista de actores después de la creación
+            fetchActores();
+        } catch (error) {
+            console.error('Error al crear el actor:', error);
+
+            // Mostrar alerta de error
+            alert('Error al crear el actor. Por favor, inténtalo de nuevo.');
+        }
+    };
+
     const handleButton1Click = () => {
-        // Lógica para el botón 1
-        console.log('Botón 1 clickeado');
-    };
-
-    const handleButton2Click = () => {
-        // Lógica para el botón 2
-        console.log('Botón 2 clickeado');
-    };
-
-    const handleButton3Click = () => {
-        // Lógica para el botón 3
-        console.log('Botón 3 clickeado');
+        setShowCrearActorInputs(true);
     };
 
     const handleButton4Click = () => {
         setShowEliminarInput(true); // Mostrar input y botón de eliminación
-    };
-
-    const handleButton5Click = () => {
-        // Lógica para el botón 5
-        console.log('Botón 5 clickeado');
-    };
-
-    const handleButton6Click = () => {
-        // Lógica para el botón 6
-        console.log('Botón 6 clickeado');
-    };
-
-    const handleButton7Click = () => {
-        // Lógica para el botón 7
-        console.log('Botón 7 clickeado');
     };
 
     return (
@@ -110,15 +131,52 @@ const Actores = () => {
                     </div>
                 )}
 
+                {/* Mostrar inputs para crear un nuevo actor si showCrearActorInputs es true */}
+                {showCrearActorInputs && (
+                    <div>
+                        <input
+                            type="text"
+                            name="nombre"
+                            value={nuevoActor.nombre}
+                            onChange={handleInputChange}
+                            placeholder="Nombre del actor"
+                        />
+                        <input
+                            type="text"
+                            name="apellido"
+                            value={nuevoActor.apellido}
+                            onChange={handleInputChange}
+                            placeholder="Apellido del actor"
+                        />
+                        <input
+                            type="text"
+                            name="yearBorn"
+                            value={nuevoActor.yearBorn}
+                            onChange={handleInputChange}
+                            placeholder="Año de nacimiento"
+                        />
+                        <input
+                            type="text"
+                            name="nacionalidad"
+                            value={nuevoActor.nacionalidad}
+                            onChange={handleInputChange}
+                            placeholder="Nacionalidad"
+                        />
+                        <input
+                            type="text"
+                            name="premios"
+                            value={nuevoActor.premios}
+                            onChange={handleInputChange}
+                            placeholder="Premios"
+                        />
+                        <button onClick={handleCrearNuevoActor}>Crear</button>
+                    </div>
+                )}
+
                 {/* Botones adicionales */}
                 <div>
                     <button onClick={handleButton1Click}>Crear nodo con 1 label</button>
-                    <button onClick={handleButton2Click}>Crear nodo con 2+ labels</button>
-                    <button onClick={handleButton3Click}>Crear nodo con propiedades</button>
                     <button onClick={handleButton4Click}>Eliminar 1 nodo</button>
-                    <button onClick={handleButton5Click}>Eliminar multiples nodos</button>
-                    <button onClick={handleButton6Click}>Eliminar 1 relación</button>
-                    <button onClick={handleButton7Click}>Eliminar multiples relaciones</button>
                 </div>
             </div>
             <table className={styles.actorTable}>
